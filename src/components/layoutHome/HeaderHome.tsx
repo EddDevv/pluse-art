ea1ef66b7075ab458e30fee894d3a39aae5031ea
mishota <mishota.o@gmail.com>
2023-06-20 00:19:42 +0300

    burger menu

--- src/components/layoutHome/HeaderHome.tsx
+++ src/components/layoutHome/HeaderHome.tsx
@@ -5,15 +5,19 @@ import Avatar from "../../assets/images/avatar.png";
 import styles from "./LayoutHome.module.scss";
 import { NavLink } from "react-router-dom";
 import { menuItems } from "../../assets/consts/consts";
+import { useMediaQuery } from "@chakra-ui/react";
+import SideBarMobile from "./SideBarMobile";
 
 const HeaderHome = () => {
+  const [isLagerThan1050] = useMediaQuery("(min-width: 1050px)");
+
   return (
     <div
       className={
         window.location.pathname === "/"
-          // ? `${styles.container} ${styles.container_main}`
-          // : styles.container
-          ? `${styles.container} ${styles.container_main}`
+          ? // ? `${styles.container} ${styles.container_main}`
+            // : styles.container
+            `${styles.container} ${styles.container_main}`
           : styles.container
       }
     >
@@ -27,29 +31,35 @@ const HeaderHome = () => {
         </div>
       </NavLink>
 
-      <div className={styles.nav_container}>
-        {menuItems.map((elem) => (
-          <div className={styles.item} key={elem.id}>
-            <NavLink
-              to={elem.to}
-              className={({ isActive }) =>
-                isActive ? styles.active_item : styles.item
-              }
-            >
-              {elem.name}
-            </NavLink>
+      {isLagerThan1050 ? (
+        <>
+          <div className={styles.nav_container}>
+            {menuItems.map((elem) => (
+              <div className={styles.item} key={elem.id}>
+                <NavLink
+                  to={elem.to}
+                  className={({ isActive }) =>
+                    isActive ? styles.active_item : styles.item
+                  }
+                >
+                  {elem.name}
+                </NavLink>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <div className={styles.personal_container}>
-        <div className={styles.link_to_pa}>Личный кабинет</div>
-        <div>
-          <img src={Avatar} alt="" />
-        </div>
-        <div>|</div>
-        <div>RU</div>
-      </div>
+          <div className={styles.personal_container}>
+            <div className={styles.link_to_pa}>Личный кабинет</div>
+            <div>
+              <img src={Avatar} alt="" />
+            </div>
+            <div>|</div>
+            <div>RU</div>
+          </div>
+        </>
+      ) : (
+        <SideBarMobile />
+      )}
     </div>
   );
 };
---
+++ src/components/layoutHome/SideBarMobile.tsx
@@ -0,0 +1,121 @@
+// import Logo from "../../../../assets/images/Logo.png";
+import styles from "./LayoutHome.module.scss";
+import React, { FC, useRef } from "react";
+import IconUser from "../../assets/images/avatar.png";
+
+import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import Logo from "../../assets/images/Logo.png";
+import { menuItems } from "../../assets/consts/consts";
+
+const SideBarMobile: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  // const btnRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <>
+      <Box>
+        <Button
+          height={12}
+          width={12}
+          onClick={onOpen}
+          // ref={btnRef}
+          as={IconButton}
+          variant="solid"
+          icon={<HamburgerIcon />}
+        >
+          Open
+        </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          // size="md"
+          closeOnEsc={true}
+          closeOnOverlayClick={true}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton color="white" />
+
+            <Box
+              p={4}
+              // bgColor={"rgba(128, 192, 192, 0.2)"}
+              className={styles.drawer_body}
+              fontFamily="Inter"
+              // overflowY="scroll"
+              h="100%"
+              color="black"
+              fontSize={25}
+            >
+              <NavLink to="/">
+                <div
+                  className={styles.logo_container}
+                  style={{ fontSize: "30px", textAlign: "center", width: "100%", padding: "30px 20px 50px 20px" }}
+                  onClick={onClose}
+                >
+                  <div>PUSLE</div>
+                  <div>
+                    <img src={Logo} alt="" />
+                  </div>
+                  <div className={styles.orange_color}>ART</div>
+                </div>
+              </NavLink>
+
+              <Box className={styles.container_double} m={2}>
+                <img src={IconUser} alt="IconUser" style={{width: "20px", height: "20px"}} />
+                <div className="orange_text" style={{fontWeight:"bold", textShadow: "2px 2px 4px black"}}>
+                  <NavLink to="/login"> Личный кабинет</NavLink>
+                </div>
+              </Box>
+              <Box className={styles.container_double} m={2}>
+                <div>
+                  <select className={styles.select}>
+                    <option className={styles.option}>Рус</option>
+                  </select>
+                </div>
+              </Box>
+
+              <Flex
+                onClick={onClose}
+                ml={12}
+                direction="column"
+                justifyContent="space-between"
+                align="flex-start"
+              >
+                {menuItems.map((elem) => (
+                  <Text m={"2"} fontSize="16px" key={elem.id}>
+                    <NavLink
+                      to={elem.to}
+                      className={({ isActive }) =>
+                        isActive ? styles.active_item : styles.item
+                      }
+                    >
+                      {elem.name}
+                    </NavLink>
+                  </Text>
+                ))}
+              </Flex>
+            </Box>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </>
+  );
+};
+
+export default SideBarMobile;
